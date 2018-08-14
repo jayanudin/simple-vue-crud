a3470e0630429b8d3372496c80e0bb3b59882342
jayanudin <jayanudin15@gmail.com>
2018-08-14 21:31:05 +0700

    update crud

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Person from '@/components/Person'
-import CreatePerson from '@/components/CreatePerson'
 
 Vue.use(Router)
 
@@ -17,11 +16,6 @@ export default new Router({
     	path: '/person',
     	name: 'Person',
     	component: Person
-    },
-    {
-    	path: '/create-person',
-    	name: 'CreatePerson',
-    	component: CreatePerson
     }
   ]
 })
